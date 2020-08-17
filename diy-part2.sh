f3930451e675b28a2ec4c3d637f012c85f7b6a12
Mr_zhaohy <zhy1@erongdu.com>
2020-08-17 10:55:34 +0800

    更新依赖

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,14 +11,16 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 #OpenClash
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 #AdGuardHome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #wireless
-git clone https://github.com/maxlicheng/phicomm-n1-wireless.git package/base-files/files/etc/modules.d
+#git clone https://github.com/maxlicheng/phicomm-n1-wireless.git package/base-files/files/etc/modules.d
 #京东签到
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/node-request.git package/node-request
+#mknop
+git clone https://github.com/tuanqing/install-program package/install-program
 
